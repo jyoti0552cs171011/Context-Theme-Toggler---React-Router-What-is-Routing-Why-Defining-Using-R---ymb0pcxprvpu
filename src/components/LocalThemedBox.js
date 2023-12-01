dbf435c99f5dd63a5f1b6454be6526a1d09ef026
jyoti0552cs171011 <74132402+jyoti0552cs171011@users.noreply.github.com>
2023-12-01 17:58:34 +0530

    Update LocalThemedBox.js

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -1,14 +1,28 @@
-import React from 'react';
-
+import React, { useContext, useState, useEffect } from 'react';
+import { ThemeContext } from './ThemeProvider';
 
 const LocalThemedBox = () => {
+  const { globalTheme, setGlobalTheme } = useContext(ThemeContext);
+  const [localTheme, setLocalTheme] = useState(globalTheme);
 
-return(
-    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
-        {/* Write code below this line */}
-        
+  useEffect(() => {
+    setLocalTheme(globalTheme);
+  }, [globalTheme]);
+
+  const toggleLocalTheme = () => {
+    setLocalTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <div className={`bg-${localTheme}`} style={{ width: '200px', height: '200px', border: '2px solid green' }} id="local-themed-box">
+      <p className={`txt-${localTheme}`} id="local-themed-text-container">
+        Some Text
+      </p>
+      <button className={`btn-${localTheme}`} id="local-theme-toggler" onClick={toggleLocalTheme}>
+        Toggle local theme to {localTheme === 'light' ? 'dark' : 'light'}
+      </button>
     </div>
-)
-}
+  );
+};
 
-export { LocalThemedBox }
+export { LocalThemedBox };
